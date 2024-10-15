99f76c3648f063e681c7b6ac5067e4d61428a484
AldJayR <aldwinjzs3@gmail.com>
2024-10-15 21:02:32 +0800

    added colors ansi

---
+++ colors.h
@@ -0,0 +1,79 @@
+#ifndef COLORS_H_INCLUDED
+#define COLORS_H_INCLUDED
+
+// Reset
+#define RESET   "\033[0m"
+
+// Regular Colors
+#define BLACK   "\033[30m"      // Black
+#define RED     "\033[31m"      // Red
+#define GREEN   "\033[32m"      // Green
+#define YELLOW  "\033[33m"      // Yellow
+#define BLUE    "\033[34m"      // Blue
+#define MAGENTA "\033[35m"      // Magenta
+#define CYAN    "\033[36m"      // Cyan
+#define WHITE   "\033[37m"      // White
+
+// Bold Colors
+#define BBLACK   "\033[1;30m"   // Bold Black
+#define BRED     "\033[1;31m"   // Bold Red
+#define BGREEN   "\033[1;32m"   // Bold Green
+#define BYELLOW  "\033[1;33m"   // Bold Yellow
+#define BBLUE    "\033[1;34m"   // Bold Blue
+#define BMAGENTA "\033[1;35m"   // Bold Magenta
+#define BCYAN    "\033[1;36m"   // Bold Cyan
+#define BWHITE   "\033[1;37m"   // Bold White
+#define BOLD     "\033[1m"
+
+
+// Underline Colors
+#define UBLACK   "\033[4;30m"   // Underline Black
+#define URED     "\033[4;31m"   // Underline Red
+#define UGREEN   "\033[4;32m"   // Underline Green
+#define UYELLOW  "\033[4;33m"   // Underline Yellow
+#define UBLUE    "\033[4;34m"   // Underline Blue
+#define UMAGENTA "\033[4;35m"   // Underline Magenta
+#define UCYAN    "\033[4;36m"   // Underline Cyan
+#define UWHITE   "\033[4;37m"   // Underline White
+
+// Background Colors
+#define ON_BLACK   "\033[40m"   // Black Background
+#define ON_RED     "\033[41m"   // Red Background
+#define ON_GREEN   "\033[42m"   // Green Background
+#define ON_YELLOW  "\033[43m"   // Yellow Background
+#define ON_BLUE    "\033[44m"   // Blue Background
+#define ON_MAGENTA "\033[45m"   // Magenta Background
+#define ON_CYAN    "\033[46m"   // Cyan Background
+#define ON_WHITE   "\033[47m"   // White Background
+
+// High Intensity Colors
+#define IBLACK   "\033[0;90m"   // High Intensity Black
+#define IRED     "\033[0;91m"   // High Intensity Red
+#define IGREEN   "\033[0;92m"   // High Intensity Green
+#define IYELLOW  "\033[0;93m"   // High Intensity Yellow
+#define IBLUE    "\033[0;94m"   // High Intensity Blue
+#define IMAGENTA "\033[0;95m"   // High Intensity Magenta
+#define ICYAN    "\033[0;96m"   // High Intensity Cyan
+#define IWHITE   "\033[0;97m"   // High Intensity White
+
+// Bold High Intensity Colors
+#define BIBLACK   "\033[1;90m"  // Bold High Intensity Black
+#define BIRED     "\033[1;91m"  // Bold High Intensity Red
+#define BIGREEN   "\033[1;92m"  // Bold High Intensity Green
+#define BIYELLOW  "\033[1;93m"  // Bold High Intensity Yellow
+#define BIBLUE    "\033[1;94m"  // Bold High Intensity Blue
+#define BIMAGENTA "\033[1;95m"  // Bold High Intensity Magenta
+#define BICYAN    "\033[1;96m"  // Bold High Intensity Cyan
+#define BIWHITE   "\033[1;97m"  // Bold High Intensity White
+
+// High Intensity Backgrounds
+#define ON_IBLACK   "\033[0;100m"   // Black Background
+#define ON_IRED     "\033[0;101m"   // Red Background
+#define ON_IGREEN   "\033[0;102m"   // Green Background
+#define ON_IYELLOW  "\033[0;103m"   // Yellow Background
+#define ON_IBLUE    "\033[0;104m"   // Blue Background
+#define ON_IMAGENTA "\033[0;105m"   // Magenta Background
+#define ON_ICYAN    "\033[0;106m"   // Cyan Background
+#define ON_IWHITE   "\033[0;107m"   // White Background
+
+#endif // COLORS_H_INCLUDED
